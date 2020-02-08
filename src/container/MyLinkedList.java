7266936ad9fd0ea290242535faef4df15e094ba6
Lazyb0x <gym1998421@gmail.com>
2020-02-08 21:23:56 +0800

    fix errors

--- src/container/MyLinkedList.java
+++ src/container/MyLinkedList.java
@@ -8,17 +8,19 @@ public class MyLinkedList {
 		//开头添加的
 		list.push("hello");
 		list.push("how");
+	    list.addFirst("i");
 		
 		//末尾添加的
 		list.add("are");
 		list.offer("you");
-		list.addFirst("i");
 		list.addLast("fine");
 		
 		System.out.println(list);
 		System.out.println(list.pop());		//开头
 		System.out.println(list);
 		System.out.println(list.poll());	//开头
+		System.out.println(list);
+		System.out.println(list.removeLast());
 		System.out.println(list);
 		System.out.println(list.getLast());
 		System.out.println(list.peekLast());
